f8ea45e169c471ca5cbfc84499e2ee1991c0d787
Parviz1999_yg <parvizshuhratugli@gmail.com>
2026-08-21 23:23:15 +0500

    BEZSESSIYA

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
 from pyrogram import Client, filters
 from config import BOT_TOKEN, API_ID, API_HASH
 
-bitik=Client("Bitik_uz", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
+bitik=Client("Bitik_uz", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, in_memory=True)
